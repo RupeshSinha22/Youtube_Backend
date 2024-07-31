dde24bf79b80d439c9f2ab2e816024ea0512e026
Rupesh_Kumar <rupuom7@gmail.com>
2024-07-31 20:02:58 +0530

    refactor: Update user controller to use refreshToken instead of newRefreshToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -200,16 +200,16 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
             secure:true
         }
     
-        const{accessToken,newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+        const{accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
     
         return res
         .status(200)
         .cookie("accessToken",accessToken,options)
-        .cookie("refreshToken",newRefreshToken,options)
+        .cookie("refreshToken",refreshToken,options)
         .json(
             new ApiResponse(
                 200,
-                {accessToken,refreshToken:newRefreshToken},
+                {accessToken,refreshToken},
                 "Access token refreshed"
             )
         )
